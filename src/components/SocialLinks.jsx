dbcc2c14410268962428eb05be17b7f03cc0b71f
maria zamora <maza@marias-Mac-mini.local>
2023-05-17 21:58:08 +0200

    Social links added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,22 +1,68 @@
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import {HighOutlineMail} from 'react-icons/hi';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
-
-
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
+
+    const links = [
+        {
+            id: 1,
+            child: (
+                <>
+                LinkedIn <FaLinkedin size={25} />
+                </> 
+            ),
+            href: 'https://www.linkedin.com/in/maria-z-b21929107/',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                Github <FaGithub size={25} />
+                </> 
+            ),
+            href: 'https://github.com/zamariac'
+        },
+        {
+            id: 3,
+            child: (
+                <>
+                Email <HiOutlineMail size={25} />
+                </> 
+            ),
+            href: 'mailto:zamora.mariac@gmail.com'
+        },
+        {
+            id: 4,
+            child: (
+                <>
+                CV <BsFillPersonLinesFill size={25} />
+                </> 
+            ),
+            href: '/resume.pdf',
+            download: true,
+            style: 'rounded-br-md'
+        }
+    ];
+
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden md:flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray'>
-                <a href="" className='flex justify-between items-center w-full text-white'>
-                {""}
-                    <>
-                    LinkedIn <FaLinkedin size={25} />
-                    </>  
+            {links.map(({id, child, href, style, download}) => (
+            <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + style}>
+                <a 
+                    href={href} 
+                    className='flex justify-between items-center w-full text-white'
+                    target="_blank"
+                    rel="noreferrer" 
+                    download={download}
+                >
+                    {child} 
                 </a>
             </li>
+            ))}
         </ul>
     </div>
   );
