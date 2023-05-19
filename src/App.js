e9e6b19dee26ea385637f3cb9d47417e7a430a37
maria zamora <maza@marias-Mac-mini.local>
2023-05-19 17:08:29 +0200

    Techstack added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import SocialLinks from './components/SocialLinks';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
       <SocialLinks />
     </div>
   );
