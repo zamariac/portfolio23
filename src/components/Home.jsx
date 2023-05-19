54fa53048a4f9db87efaaa536ce92727139b5548
maria zamora <maza@marias-Mac-mini.local>
2023-05-19 21:55:08 +0200

    Contact for added - React scroll added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,50 @@
-import React from 'react';
-import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
-import HeroImage from '../assets/heroImage.png';
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
-function Home() {
+const Home = () => {
   return (
-    <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>Hi! I'm a Front End Developer</h2>
-                <p className='text-gray-500 py-4 max-w-md'> I have 5 years of web development experience working with tech companies, creative agencies and with private clients. I love working with designers and building beautiful things!
-                </p>
-                <div>
-                    <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>Portfolio
-                        <span className="hover:rotate-90 duration-300">
-                            <MdKeyboardDoubleArrowRight size={20} className="ml-1" />
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <div>
-                <img 
-                    src={HeroImage} 
-                    alt='me'
-                    className="rounded-2xl mx-auto w-2/3 md:w-full" 
-                />
-            </div>
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I have 8 years of experience building and desgining software.
+            Currently, I love to work on web application using technologies like
+            React, Tailwind, Next JS and GraphQL.
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
         </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,71 +1,77 @@
-import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-
-    const links = [
-        {
-            id: 1,
-            child: (
-                <>
-                LinkedIn <FaLinkedin size={25} />
-                </> 
-            ),
-            href: 'https://www.linkedin.com/in/maria-z-b21929107/',
-            style: 'rounded-tr-md'
-        },
-        {
-            id: 2,
-            child: (
-                <>
-                Github <FaGithub size={25} />
-                </> 
-            ),
-            href: 'https://github.com/zamariac'
-        },
-        {
-            id: 3,
-            child: (
-                <>
-                Email <HiOutlineMail size={25} />
-                </> 
-            ),
-            href: 'mailto:zamora.mariac@gmail.com'
-        },
-        {
-            id: 4,
-            child: (
-                <>
-                CV <BsFillPersonLinesFill size={25} />
-                </> 
-            ),
-            href: '/resume.pdf',
-            download: true,
-            style: 'rounded-br-md'
-        }
-    ];
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://linkedin.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/theyashpatel",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:foo@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
 
   return (
-    <div className='hidden md:flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-            {links.map(({id, child, href, style, download}) => (
-            <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + style}>
-                <a 
-                    href={href} 
-                    className='flex justify-between items-center w-full text-white'
-                    target="_blank"
-                    rel="noreferrer" 
-                    download={download}
-                >
-                    {child} 
-                </a>
-            </li>
-            ))}
-        </ul>
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default SocialLinks
+export default SocialLinks;
