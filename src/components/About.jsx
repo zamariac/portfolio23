208e63516214609d24ba6a29004582b1ece0a022
maria zamora <maza@marias-Mac-mini.local>
2023-05-19 16:35:36 +0200

    Portfolio grid added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
   return (
     <div 
         name="about" 
-        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'
+        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white py-3' 
     >
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8">
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About Me</p>
             </div>
-            <p className="text-xl mt-20">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus minima eaque aperiam qui cum veniam cupiditate, dolor laudantium quisquam doloremque reprehenderit minus rem nostrum, est eum vero possimus accusamus? Fuga!</p>
+            <p className="text-xl mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus minima eaque aperiam qui cum veniam cupiditate, dolor laudantium quisquam doloremque reprehenderit minus rem nostrum, est eum vero possimus accusamus? Fuga!</p>
         <br />
         <p className="text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus minima eaque aperiam qui cum veniam cupiditate, dolor laudantium quisquam doloremque reprehenderit minus rem nostrum, est eum vero possimus accusamus? Fuga!</p>
         </div>
